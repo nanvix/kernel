b9d24f66b3d031c8486507d4ce5bf06a82a62833
Theldus <davidsondfgl@gmail.com>
2018-12-20 04:22:04 -0200

    [or1k] Feature: OR1K Header

---
+++ include/arch/or1k/or1k.h
@@ -0,0 +1,42 @@
+/*
+ * MIT License
+ *
+ * Copyright(c) 2011-2018 Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
+ *              2017-2018 Davidson Francis     <davidsondfgl@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef ARCH_OR1K_OR1K_H_
+#define ARCH_OR1K_OR1K_H_
+
+
+	#ifndef TARGET_OR1K_PC_H_
+	#error "include <target/or1k/pc.h> instead"
+	#endif
+
+	/**
+	 * @defgroup or1k Architecture
+	 * @ingroup or1k-pc
+	 */
+	#include <arch/or1k/core.h>
+	#include <arch/or1k/cpu.h>
+	#include <arch/or1k/mem.h>
+
+#endif /* ARCH_OR1K_OR1K_H_ */
