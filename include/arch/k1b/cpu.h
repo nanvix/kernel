2270fad0849d0216652c024893d0029f3a103272
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2019-01-24 16:06:34 +0100

    Feature: HAL Spinlock Interface Implementation
    In this commit, I introduce the implementation of the HAL Spinlock
    interface for the k1b architecture.

--- include/arch/k1b/cpu.h
+++ include/arch/k1b/cpu.h
@@ -38,7 +38,14 @@
 	 */
 	/**@{*/
 	#define __hal_cpu_get_num_cores
+	#define __spinlock_t          /**< @p spinlock_t      */
+	#define __spinlock_init_fn    /**< spinlock_init()    */
+	#define __spinlock_lock_fn    /**< spinlock_lock()    */
+	#define __spinlock_trylock_fn /**< spinlock_trylock() */
+	#define __spinlock_unlock_fn  /**< spinlock_unlock()  */
 	/**@}*/
+
+	#include <nanvix/const.h>
 
 	/**
 	 * @brief Number of cores in the k1b processor.
@@ -70,6 +77,113 @@
 	static inline int hal_cpu_get_num_cores(void)
 	{
 		return (k1b_cpu_get_num_cores());
+	}
+	/**@endcond*/
+
+	/**
+	 * @brief Gets the number of cores.
+	 *
+	 * @returns The number of cores in the underlying processor.
+	 */
+	EXTERN int hal_cpu_get_num_cores(void);
+
+/*============================================================================*
+ *                                 Spinlocks                                  *
+ *============================================================================*/
+
+	#include <HAL/hal/hal_ext.h>
+
+	/**
+	 * @brief Spinlock.
+	 */
+	typedef __k1_fspinlock_t spinlock_t;
+
+	/**
+	 * @brief Initializes a spinlock.
+	 *
+	 * @param lock Target spinlock.
+	 */
+	static inline void k1b_spinlock_init(spinlock_t *lock)
+	{
+		__k1_fspinlock_init(lock);
+	}
+
+	/**
+	 * @see k1b_spinlock_init()
+	 *
+	 * @cond k1b
+	 */
+	static inline void spinlock_init(spinlock_t *lock)
+	{
+		k1b_spinlock_init(lock);
+	}
+	/**@endcond*/
+
+	/**
+	 * @brief Locks a spinlock.
+	 *
+	 * @param lock Target spinlock.
+	 */
+	static inline void k1b_spinlock_lock(spinlock_t *lock)
+	{
+		__k1_fspinlock_lock(lock);
+	}
+
+	/**
+	 * @see k1b_spinlock_lock()
+	 *
+	 * @cond k1b
+	 */
+	static inline void spinlock_lock(spinlock_t *lock)
+	{
+		k1b_spinlock_lock(lock);
+	}
+	/**@endcond*/
+
+	/**
+	 * @brief Attempts to lock a spinlock.
+	 *
+	 * @param lock Target spinlock.
+	 *
+	 * @returns Upon successful completion, the spinlock pointed to by
+	 * @p lock is locked and zero is returned. Upon failure, non-zero
+	 * is returned instead, and the lock is not acquired by the
+	 * caller.
+	 */
+	static inline int k1b_spinlock_trylock(spinlock_t *lock)
+	{
+		return (__k1_fspinlock_trylock(lock));
+	}
+
+	/**
+	 * @see k1b_spinlock_trylock()
+	 *
+	 * @cond k1b
+	 */
+	static inline int spinlock_trylock(spinlock_t *lock)
+	{
+		return (k1b_spinlock_trylock(lock));
+	}
+	/**@endcond*/
+
+	/**
+	 * @brief Unlocks a spinlock.
+	 *
+	 * @param lock Target spinlock.
+	 */
+	static inline void k1b_spinlock_unlock(spinlock_t *lock)
+	{
+		__k1_fspinlock_unlock(lock);
+	}
+
+	/**
+	 * @see k1b_spinlock_unlock()
+	 *
+	 * @cond k1b
+	 */
+	static inline void spinlock_unlock(spinlock_t *lock)
+	{
+		k1b_spinlock_unlock(lock);
 	}
 	/**@endcond*/
 
