f42d2768872f40c1e46e436fa33045888e9ae5b4
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2019-04-24 14:08:52 +0200

    Enhancement: Bumping Dev Scripts

--- tools/dev/arch/i386.sh
+++ tools/dev/arch/i386.sh
@@ -1,9 +1,7 @@
 #
 # MIT License
 #
-# Copyright(c) 2011-2018 Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
-#              2016-2018 Davidson Francis     <davidsondfgl@gmail.com>
-#              2016-2017 Subhra Sarkar        <rurtle.coder@gmail.com>
+# Copyright(c) 2011-2019 The Maintainers of Nanvix
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -29,26 +27,32 @@ export CURDIR=`pwd`
 export WORKDIR=$CURDIR/tools/dev/toolchain/i386
 export PREFIX=$WORKDIR
 export TARGET=i386-elf
-
-mkdir -p $WORKDIR
-cd $WORKDIR
+export COMMIT=a28342821bf8784fd03abbe577eae64b805cc1f9
 
 # Retrieve the number of processor cores
 NCORES=`grep -c ^processor /proc/cpuinfo`
 
-# Get binutils, GDB and GCC.
-if [ ! "$(ls -A $WORKDIR)" ];
-then
-	git submodule update --init $WORKDIR
-fi
+mkdir -p $WORKDIR
+cd $WORKDIR
+
+# Get toolchain.
+wget "https://github.com/nanvix/toolchain/archive/$COMMIT.zip"
+unzip $COMMIT.zip
+mv toolchain-$COMMIT/* .
+
+# Cleanup
+rm -rf toolchain-$COMMIT
+rm -rf $COMMIT.zip
 
 # Build binutils.
 cd binutils*/
 ./configure --target=$TARGET --prefix=$PREFIX --disable-nls
 make -j $NCORES all
 make install
-git checkout .
-git clean -f -d
+
+# Cleanup.
+cd $WORKDIR
+rm -rf binutils*
 
 # Build GCC.
 cd $WORKDIR
@@ -61,8 +65,10 @@ make -j $NCORES all-gcc
 make -j $NCORES all-target-libgcc
 make install-gcc
 make install-target-libgcc
-git checkout .
-git clean -f -d
+
+# Cleanup.
+cd $WORKDIR
+rm -rf gcc*
 
 # Build GDB.
 cd $WORKDIR
@@ -70,8 +76,10 @@ cd gdb*/
 ./configure --target=$TARGET --prefix=$PREFIX --with-auto-load-safe-path=/ --with-guile=no
 make -j $NCORES
 make install
-git checkout .
-git clean -f -d
+
+# Cleanup.
+cd $WORKDIR
+rm -rf gdb*
 
 # Back to the current folder
 cd $CURDIR
