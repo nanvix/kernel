45e72059ef4140b9803745334343a1aabb38690e
Antoine Saget <antoinesaget19@gmail.com>
2019-06-25 16:39:59 +0200

    [x86] Work In Progress: PCI driver debugging

--- src/kernel/dev/pci.c
+++ src/kernel/dev/pci.c
@@ -25,7 +25,8 @@
 /*
  * PCI Init, filling size for each field in config space
  * */
-#include <pci.h>
+#include <dev/pci.h>
+#include <pmio.h>
 
 uint32_t pci_size_map[100];
 union pci_dev dev_zero = {0};
@@ -34,7 +35,7 @@ union pci_dev dev_zero = {0};
  * Given a pci device(32-bit vars containing info about bus, device number, and function number), a field(what u want to read from the config space)
  * Read it for me !
  * */
-uint32_t pci_read(union pci_dev dev, uint32_t field)
+uint32_t dev_pci_read(union pci_dev dev, uint32_t field)
 {
     // Only most significant 6 bits of the field
     dev.field_num = (field & 0xFC) >> 2;
@@ -66,7 +67,7 @@ uint32_t pci_read(union pci_dev dev, uint32_t field)
 /*
  * Write pci field
  * */
-void pci_write(union pci_dev dev, uint32_t field, uint32_t value)
+void dev_pci_write(union pci_dev dev, uint32_t field, uint32_t value)
 {
     dev.field_num = (field & 0xFC) >> 2;
     dev.enable = 1;
@@ -79,26 +80,26 @@ void pci_write(union pci_dev dev, uint32_t field, uint32_t value)
 /*
  * Get device type (i.e, is it a bridge, ide controller ? mouse controller? etc)
  * */
-uint32_t get_device_type(union pci_dev dev)
+uint32_t dev_pci_get_device_type(union pci_dev dev)
 {
-    uint32_t t = pci_read(dev, PCI_CLASS) << 8;
-    return t | pci_read(dev, PCI_SUBCLASS);
+    uint32_t t = dev_pci_read(dev, PCI_CLASS) << 8;
+    return t | dev_pci_read(dev, PCI_SUBCLASS);
 }
 
 /*
  * Get secondary bus from a PCI bridge device
  * */
-uint32_t get_secondary_bus(union pci_dev dev)
+uint32_t dev_pci_get_secondary_bus(union pci_dev dev)
 {
-    return pci_read(dev, PCI_SECONDARY_BUS);
+    return dev_pci_read(dev, PCI_SECONDARY_BUS);
 }
 
 /*
  * Is current device an end point ? PCI_HEADER_TYPE 0 is end point
  * */
-uint32_t pci_reach_end(union pci_dev dev)
+uint32_t dev_pci_reach_end(union pci_dev dev)
 {
-    uint32_t t = pci_read(dev, PCI_HEADER_TYPE);
+    uint32_t t = dev_pci_read(dev, PCI_HEADER_TYPE);
     return !t;
 }
 
@@ -112,7 +113,7 @@ uint32_t pci_reach_end(union pci_dev dev)
 /*
  * Scan function
  * */
-union pci_dev pci_scan_function(uint16_t vendor_id, uint16_t device_id, uint32_t bus, uint32_t device, uint32_t function, int device_type)
+union pci_dev dev_pci_scan_function(uint16_t vendor_id, uint16_t device_id, uint32_t bus, uint32_t device, uint32_t function, uint32_t device_type)
 {
     union pci_dev dev = {0};
     dev.bus_num = bus;
@@ -120,16 +121,16 @@ union pci_dev pci_scan_function(uint16_t vendor_id, uint16_t device_id, uint32_t
     dev.function_num = function;
 
     // If it's a PCI Bridge device, get the bus it's connected to and keep searching
-    if (get_device_type(dev) == PCI_TYPE_BRIDGE)
+    if (dev_pci_get_device_type(dev) == PCI_TYPE_BRIDGE)
     {
-        pci_scan_bus(vendor_id, device_id, get_secondary_bus(dev), device_type);
+        dev_pci_scan_bus(vendor_id, device_id, get_secondary_bus(dev), device_type);
     }
 
     // If type matches, we've found the device, just return it
     if (device_type == -1 || device_type == get_device_type(dev))
     {
-        uint32_t devid = pci_read(dev, PCI_DEVICE_ID);
-        uint32_t vendid = pci_read(dev, PCI_VENDOR_ID);
+        uint32_t devid = dev_pci_read(dev, PCI_DEVICE_ID);
+        uint32_t vendid = dev_pci_read(dev, PCI_VENDOR_ID);
         if (devid == device_id && vendor_id == vendid)
             return dev;
     }
@@ -139,27 +140,27 @@ union pci_dev pci_scan_function(uint16_t vendor_id, uint16_t device_id, uint32_t
 /*
  * Scan device
  * */
-union pci_dev pci_scan_device(uint16_t vendor_id, uint16_t device_id, uint32_t bus, uint32_t device, int device_type)
+union pci_dev dev_pci_scan_device(uint16_t vendor_id, uint16_t device_id, uint32_t bus, uint32_t device, uint32_t device_type)
 {
     union pci_dev dev = {0};
     dev.bus_num = bus;
     dev.device_num = device;
 
-    if (pci_read(dev, PCI_VENDOR_ID) == PCI_NONE)
+    if (dev_pci_read(dev, PCI_VENDOR_ID) == PCI_NONE)
         return dev_zero;
 
-    union pci_dev t = pci_scan_function(vendor_id, device_id, bus, device, 0, device_type);
+    union pci_dev t = dev_pci_scan_function(vendor_id, device_id, bus, device, 0, device_type);
     if (t.bits)
         return t;
 
-    if (pci_reach_end(dev))
+    if (dev_pci_reach_end(dev))
         return dev_zero;
 
     for (int function = 1; function < FUNCTION_PER_DEVICE; function++)
     {
-        if (pci_read(dev, PCI_VENDOR_ID) != PCI_NONE)
+        if (dev_pci_read(dev, PCI_VENDOR_ID) != PCI_NONE)
         {
-            t = pci_scan_function(vendor_id, device_id, bus, device, function, device_type);
+            t = dev_pci_scan_function(vendor_id, device_id, bus, device, function, device_type);
             if (t.bits)
                 return t;
         }
@@ -169,11 +170,11 @@ union pci_dev pci_scan_device(uint16_t vendor_id, uint16_t device_id, uint32_t b
 /*
  * Scan bus
  * */
-union pci_dev pci_scan_bus(uint16_t vendor_id, uint16_t device_id, uint32_t bus, int device_type)
+union pci_dev dev_pci_scan_bus(uint16_t vendor_id, uint16_t device_id, uint32_t bus, uint32_t device_type)
 {
     for (int device = 0; device < DEVICE_PER_BUS; device++)
     {
-        union pci_dev t = pci_scan_device(vendor_id, device_id, bus, device, device_type);
+        union pci_dev t = dev_pci_scan_device(vendor_id, device_id, bus, device, device_type);
         if (t.bits)
             return t;
     }
@@ -183,16 +184,16 @@ union pci_dev pci_scan_bus(uint16_t vendor_id, uint16_t device_id, uint32_t bus,
 /*
  * Device driver use this function to get its device object(given unique vendor id and device id)
  * */
-union pci_dev pci_get_device(uint16_t vendor_id, uint16_t device_id, int device_type)
+union pci_dev dev_pci_get_device(uint16_t vendor_id, uint16_t device_id, uint32_t device_type)
 {
 
-    union pci_dev t = pci_scan_bus(vendor_id, device_id, 0, device_type);
+    union pci_dev t = dev_pci_scan_bus(vendor_id, device_id, 0, device_type);
     if (t.bits)
         return t;
 
     // Handle multiple pci host controllers
 
-    if (pci_reach_end(dev_zero)) // ?
+    if (dev_pci_reach_end(dev_zero)) // ?
     {
         printf("PCI Get device failed...\n");
     }
@@ -204,14 +205,14 @@ union pci_dev pci_get_device(uint16_t vendor_id, uint16_t device_id, int device_
 
         if (pci_read(dev, PCI_VENDOR_ID) == PCI_NONE)
             break; */
-        t = pci_scan_bus(vendor_id, device_id, bus, device_type);
+        t = dev_pci_scan_bus(vendor_id, device_id, bus, device_type);
         if (t.bits)
             return t;
     }
     return dev_zero;
 }
 
-void pci_init()
+void dev_pci_init()
 {
     // Init size map
     pci_size_map[PCI_VENDOR_ID] = 2;
