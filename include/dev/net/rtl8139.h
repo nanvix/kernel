1ce76e850a266b09e0fd772b529b2c7390905917
Antoine Saget <antoinesaget19@gmail.com>
2019-06-25 14:11:38 +0200

    [x86] Add empty c files and headers for pci and rtl8139 drivers

--- include/dev/net/rtl8139.h
+++ include/dev/net/rtl8139.h
@@ -30,46 +30,89 @@
 #define RTL8139_H_
 
 #include <stdint.h>
-#include <nanvix/hal/hal.h>
 
+// Define some constants
 #define RTL8139_VENDOR_ID 0x10EC
 #define RTL8139_DEVICE_ID 0x8139
 
-#define RX_BUF_SIZE 8192 + 16
-/* Adding 1500 bytes when allocating to prevent overflows */
-#define RX_BUF_ALLOC_SIZE RX_BUF_SIZE + 1500 
+#define RX_BUF_SIZE 8192
 
+#define CAPR 0x38
 #define RX_READ_POINTER_MASK (~3)
 #define ROK (1 << 0)
+#define RER (1 << 1)
 #define TOK (1 << 2)
+#define TER (1 << 3)
+#define TX_TOK (1 << 15)
 
-/* Register offests */
-#define RX_BUFFER 0x30
-#define COMMAND 0x37
-#define CAPR 0x38
-#define TX_CONFIG 0x40
-#define RX_CONFIG 0x44
-#define CONFIG1 0x52
-#define INTERRUPT_MASK 0x3C
-#define INTERRUPT_STATUS 0x3E
+enum RTL8139_registers
+{
+	MAG0 = 0x00,	  // Ethernet hardware address
+	MAR0 = 0x08,	  // Multicast filter
+	TxStatus0 = 0x10, // Transmit status (Four 32bit registers)
+	TxAddr0 = 0x20,   // Tx descriptors (also four 32bit)
+	RxBuf = 0x30,
+	RxEarlyCnt = 0x34,
+	RxEarlyStatus = 0x36,
+	ChipCmd = 0x37,
+	RxBufPtr = 0x38,
+	RxBufAddr = 0x3A,
+	IntrMask = 0x3C,
+	IntrStatus = 0x3E,
+	TxConfig = 0x40,
+	RxConfig = 0x44,
+	Timer = 0x48,	// A general-purpose counter
+	RxMissed = 0x4C, // 24 bits valid, write clears
+	Cfg9346 = 0x50,
+	Config0 = 0x51,
+	Config1 = 0x52,
+	FlashReg = 0x54,
+	GPPinData = 0x58,
+	GPPinDir = 0x59,
+	MII_SMI = 0x5A,
+	HltClk = 0x5B,
+	MultiIntr = 0x5C,
+	TxSummary = 0x60,
+	MII_BMCR = 0x62,
+	MII_BMSR = 0x64,
+	NWayAdvert = 0x66,
+	NWayLPAR = 0x68,
+	NWayExpansion = 0x6A,
+
+	// Undocumented registers, but required for proper operation
+	FIFOTMS = 0x70, // FIFO Control and test
+	CSCR = 0x74,	// Chip Status and Configuration Register
+	PARA78 = 0x78,
+	PARA7c = 0x7c, // Magic transceiver parameter register
+};
+
+struct tx_desc
+{
+	uint32_t phys_addr;
+	uint32_t packet_size;
+};
 
 struct rtl8139_dev
 {
+	uint8_t bar_type;
 	uint16_t io_base;
+	uint32_t mem_base;
+	int eeprom_exist;
 	uint8_t mac_addr[6];
-	uint8_t rx_buffer[RX_BUF_ALLOC_SIZE];
-	uint8_t tx_cur;
-	uint32_t rx_cur;
+	char *rx_buffer;
+	int tx_cur;
 };
 
-EXTERN void network_test_driver();
+void net_rtl8139_send_packet(void *data, uint32_t len);
 
-void dev_net_rtl8139_init();
-void dev_net_rtl8139_send_packet(void *data, uint32_t len);
+void net_rtl8139_handler();
 
-struct rtl8139_dev* dev_net_rtl8139_get_device();
+void net_rtl8139_init();
 
-bool dev_net_rtl8139_packet_status_valid(uint16_t status);
+void net_rtl8139_read_mac_addr();
 
+void net_rtl8139_receive_packet();
+
+void net_rtl8139_get_mac_addr();
 
 #endif /* RTL8139_H_ */
