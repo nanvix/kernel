5aecdfc73b4abc24bf0ad459deda7cc8cc347123
Antoine Saget <antoinesaget19@gmail.com>
2019-07-08 17:10:16 -0300

    [qemu-x86] code style

--- include/dev/net/rtl8139.h
+++ include/dev/net/rtl8139.h
@@ -37,14 +37,14 @@
 #define RX_BUF_SIZE 8192 + 16
 #define RX_BUF_ALLOC_SIZE RX_BUF_SIZE + 1500 /* Adding 1500 bytes to prevent overflows */
 
-#define CAPR 0x38
 #define RX_READ_POINTER_MASK (~3)
 #define ROK (1 << 0)
 #define TOK (1 << 2)
 
 /* Register offests */
-#define COMMAND 0x37
 #define RX_BUFFER 0x30
+#define COMMAND 0x37
+#define CAPR 0x38
 #define RX_CONFIG 0x44
 #define CONFIG1 0x52
 #define INTERRUPT_MASK 0x3C
@@ -52,9 +52,7 @@
 
 struct rtl8139_dev
 {
-	// uint8_t bar_type;
 	uint16_t io_base;
-	// uint32_t mem_base;
 	uint8_t mac_addr[6];
 	uint8_t rx_buffer[RX_BUF_ALLOC_SIZE];
 	int tx_cur;
