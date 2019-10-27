3a3fbaadc40f3a23011acf4e6fbd8a26b8fdbb78
João Fellipe <felippe.uller@gmail.com>
2019-10-26 21:01:14 -0300

    [ikc] Enhancement: HW Sync Search

--- src/kernel/noc/sync.c
+++ src/kernel/noc/sync.c
@@ -28,6 +28,14 @@
 
 #if __TARGET_HAS_SYNC
 
+/**
+ * @brief Search types for do_sync_search().
+ */
+enum sync_search_type {
+	SYNC_SEARCH_INPUT = 0,
+	SYNC_SEARCH_OUTPUT = 1
+} resource_type_enum_t;
+
 /*============================================================================*
  * Control Structures.                                                        *
  *============================================================================*/
@@ -42,7 +50,7 @@ PRIVATE struct sync
 	int fd;                   /**< Underlying file descriptor. */
 	int type;                 /**< Sync type.                  */
 	int masternum;            /**< Node number of the ONE.     */
-	uint64_t footprint;       /**< Node ID list.               */
+	uint64_t nodeslist;       /**< Node ID list.               */
 } ALIGN(sizeof(dword_t)) synctab[(SYNC_CREATE_MAX + SYNC_OPEN_MAX)];
 
 /**
@@ -74,22 +82,88 @@ PRIVATE int do_sync_is_valid(int syncid)
 }
 
 /*============================================================================*
+ * do_sync_search()                                                        *
+ *============================================================================*/
+
+/**
+ * @name Helper Macros for do_sync_search()
+ */
+/**@{*/
+
+/**
+ * @brief Asserts an input sync.
+ */
+#define SYNC_SEARCH_IS_INPUT(mbxid,type) \
+	((type == SYNC_SEARCH_INPUT) && !resource_is_readable(&synctab[mbxid].resource))
+
+/**
+ * @brief Asserts an output sync.
+ */
+#define SYNC_SEARCH_IS_OUTPUT(mbxid,type) \
+	 ((type == SYNC_SEARCH_OUTPUT) && !resource_is_writable(&synctab[mbxid].resource))
+/**@}*/
+
+/**
+ * @brief Searches for a sync.
+ *
+ * Searches for an already existing sync in synctab.
+ *
+ * @param masternum    Logic ID of the master node.
+ * @param nodeslist    Target nodes list.
+ * @param sync_type    Type of synchronization type.
+ * @param search_type  Type of the searched resource.
+ *
+ * @returns Upon successful completion, the ID of the sync found is
+ * returned. Upon failure, a negative error code is returned instead.
+ */
+PRIVATE int do_sync_search(int masternum, uint64_t nodeslist, int sync_type, enum sync_search_type search_type)
+{
+	for (int i = 0; i < (SYNC_CREATE_MAX + SYNC_OPEN_MAX); ++i)
+	{
+		if (!resource_is_used(&synctab[i].resource))
+			continue;
+
+		if (SYNC_SEARCH_IS_INPUT(i, search_type))
+			continue;
+
+		else if (SYNC_SEARCH_IS_OUTPUT(i, search_type))
+			continue;
+
+		/* Not the same master? */
+		if (synctab[i].masternum != masternum)
+			continue;
+
+		/* Not the same node list? */
+		if (synctab[i].nodeslist != nodeslist)
+			continue;
+
+		/* Not the same type operation? */
+		if (synctab[i].type != sync_type)
+			continue;
+
+		return (i);
+	}
+
+	return (-1);
+}
+
+/*============================================================================*
  * do_sync_create()                                                           *
  *============================================================================*/
 
 /**
  * @brief Creates a synchronization point.
  *
- * @param nodes     Logic IDs of Target Nodes.
- * @param nnodes    Number of Target Nodes.
- * @param type      Type of synchronization point.
- * @param footprint Target nodes footprint.
+ * @param nodes      Logic IDs of target nodes.
+ * @param nnodes     Number of target nodes.
+ * @param type       Type of synchronization point.
+ * @param nodeslist  Target nodes list.
  *
  * @returns Upon successful completion, the ID of the newly created
  * synchronization point is returned. Upon failure, a negative error
  * code is returned instead.
  */
-PRIVATE int _do_sync_create(const int *nodes, int nnodes, int type, uint64_t footprint)
+PRIVATE int _do_sync_create(const int *nodes, int nnodes, int type, uint64_t nodeslist)
 {
 	int fd;
 	int syncid;
@@ -107,9 +181,9 @@ PRIVATE int _do_sync_create(const int *nodes, int nnodes, int type, uint64_t foo
 	/* Initialize synchronization point. */
 	synctab[syncid].fd        = fd;
 	synctab[syncid].type      = type;
-	synctab[syncid].refcount  = 1;
+	synctab[syncid].refcount  = 0;
 	synctab[syncid].masternum = nodes[0];
-	synctab[syncid].footprint = footprint;
+	synctab[syncid].nodeslist = nodeslist;
 
 	resource_set_rdonly(&synctab[syncid].resource);
 	resource_set_notbusy(&synctab[syncid].resource);
@@ -123,45 +197,22 @@ PRIVATE int _do_sync_create(const int *nodes, int nnodes, int type, uint64_t foo
 PUBLIC int do_sync_create(const int *nodes, int nnodes, int type)
 {
 	int syncid;         /* Synchronization point.  */
-	uint64_t footprint; /* Target nodes footprint. */
+	uint64_t nodeslist; /* Target nodes list. */
 
-	footprint = 0ULL;
+	nodeslist = 0ULL;
 	for (int j = 0; j < nnodes; j++)
-		footprint |= (1ULL << nodes[j]);
+		nodeslist |= (1ULL << nodes[j]);
 
 	/* Searchs for existing syncs. */
-	for (int i = 0; i < (SYNC_CREATE_MAX + SYNC_OPEN_MAX); ++i)
+	if ((syncid = do_sync_search(nodes[0], nodeslist, type, SYNC_SEARCH_INPUT)) < 0)
 	{
-		if (!resource_is_used(&synctab[i].resource))
-			continue;
-
-		if (!resource_is_readable(&synctab[i].resource))
-			continue;
-
-		/* Not the same master? */
-		if (synctab[i].masternum != nodes[0])
-			continue;
-
-		/* Not the same node list? */
-		if (synctab[i].footprint != footprint)
-			continue;
-
-		/* Not the same type operation? */
-		if (synctab[i].type != type)
-			continue;
-
-		syncid = i;
-		synctab[i].refcount++;
-
-		goto found;
+		/* Alloc a new synchronization point. */
+		syncid = _do_sync_create(nodes, nnodes, type, nodeslist);
 	}
 
-	/* Alloc a new synchronization point. */
-	syncid = _do_sync_create(nodes, nnodes, type, footprint);
+	synctab[syncid].refcount++;
 
-found:
 	dcache_invalidate();
-
 	return (syncid);
 }
 
@@ -172,10 +223,10 @@ found:
 /**
  * @brief Opens a synchronization point.
  *
- * @param nodes  Logic IDs of Target Nodes.
- * @param nnodes Number of Target Nodes.
- * @param type   Type of synchronization point.
- * @param footprint Target nodes footprint.
+ * @param nodes      Logic IDs of target nodes.
+ * @param nnodes     Number of target nodes.
+ * @param type       Type of synchronization point.
+ * @param nodeslist  Target nodes list.
  *
  * @returns Upon successful completion, the ID of the target
  * synchronization point is returned. Upon failure, a negative error
@@ -183,7 +234,7 @@ found:
  *
  * @todo Check for Invalid Remote
  */
-PRIVATE int _do_sync_open(const int *nodes, int nnodes, int type, uint64_t footprint)
+PRIVATE int _do_sync_open(const int *nodes, int nnodes, int type, uint64_t nodeslist)
 {
 	int fd;		/* File descriptor.       */
 	int syncid; /* Synchronization point. */
@@ -202,9 +253,9 @@ PRIVATE int _do_sync_open(const int *nodes, int nnodes, int type, uint64_t footp
 	/* Initialize synchronization point. */
 	synctab[syncid].fd        = fd;
 	synctab[syncid].type      = type;
-	synctab[syncid].refcount  = 1;
+	synctab[syncid].refcount  = 0;
 	synctab[syncid].masternum = nodes[0];
-	synctab[syncid].footprint = footprint;
+	synctab[syncid].nodeslist = nodeslist;
 
 	resource_set_wronly(&synctab[syncid].resource);
 	resource_set_notbusy(&synctab[syncid].resource);
@@ -218,45 +269,22 @@ PRIVATE int _do_sync_open(const int *nodes, int nnodes, int type, uint64_t footp
 PUBLIC int do_sync_open(const int *nodes, int nnodes, int type)
 {
 	int syncid;         /* Synchronization point.  */
-	uint64_t footprint; /* Target nodes footprint. */
+	uint64_t nodeslist; /* Target nodes list.      */
 
-	footprint = 0ULL;
+	nodeslist = 0ULL;
 	for (int j = 0; j < nnodes; j++)
-		footprint |= (1ULL << nodes[j]);
+		nodeslist |= (1ULL << nodes[j]);
 
 	/* Searchs for existing syncs. */
-	for (int i = 0; i < (SYNC_CREATE_MAX + SYNC_OPEN_MAX); ++i)
+	if ((syncid = do_sync_search(nodes[0], nodeslist, type, SYNC_SEARCH_OUTPUT)) < 0)
 	{
-		if (!resource_is_used(&synctab[i].resource))
-			continue;
-
-		if (!resource_is_writable(&synctab[i].resource))
-			continue;
-
-		/* Not the same master? */
-		if (synctab[i].masternum != nodes[0])
-			continue;
-
-		/* Not the same node list? */
-		if (synctab[i].footprint != footprint)
-			continue;
-
-		/* Not the same type operation? */
-		if (synctab[i].type != type)
-			continue;
-
-		syncid = i;
-		synctab[i].refcount++;
-
-		goto found;
+		/* Alloc a new synchronization point. */
+		syncid = _do_sync_open(nodes, nnodes, type, nodeslist);
 	}
 
-	/* Alloc a new synchronization point. */
-	syncid = _do_sync_open(nodes, nnodes, type, footprint);
+	synctab[syncid].refcount++;
 
-found:
 	dcache_invalidate();
-
 	return (syncid);
 }
 
@@ -286,7 +314,7 @@ PRIVATE int _do_sync_release(int syncid, int (*release_fn)(int))
 
 		synctab[syncid].fd        = -1;
 		synctab[syncid].masternum = -1;
-		synctab[syncid].footprint = 0ULL;
+		synctab[syncid].nodeslist = 0ULL;
 
 		resource_free(&syncpool, syncid);
 
