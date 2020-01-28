c0450edb59862bb45d6760de7b79e80b46a6151a
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2020-01-28 09:27:24 -0300

    Enhancement: Updating Copyright

--- src/kernel/sys/write.c
+++ src/kernel/sys/write.c
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright(c) 2011-2019 The Maintainers of Nanvix
+ * Copyright(c) 2011-2020 The Maintainers of Nanvix
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
