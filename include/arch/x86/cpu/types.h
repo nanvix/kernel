71d901c26a0500c6b3b0291a5e240b97b953aca0
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2023-07-29 14:00:45 -0300

    [x86] E: Improving Documentation

--- include/arch/x86/cpu/types.h
+++ include/arch/x86/cpu/types.h
@@ -6,29 +6,49 @@
 #ifndef ARCH_X86_CPU_TYPES_H_
 #define ARCH_X86_CPU_TYPES_H_
 
+/**
+ * @addtogroup x86-cpu-types x86 CPU Types
+ * @ingroup x86-cpu
+ *
+ * @brief x86 CPU Types
+ */
+/**@{*/
+
+/*============================================================================*
+ * Imports                                                                    *
+ *============================================================================*/
+
 #ifndef _ASM_FILE_
 #include <stdint.h>
 #endif /* _ASM_FILE_ */
+
+/*============================================================================*
+ * Constants                                                                  *
+ *============================================================================*/
 
 /**
  * @name Bit-Length of CPU Types
  */
 /**@{*/
-#define BYTE_BIT 8   /** Byte        */
-#define HWORD_BIT 16 /** Half Word   */
-#define WORD_BIT 32  /** Word        */
-#define DWORD_BIT 64 /** Double Word */
+#define BYTE_BIT 8   /** Number of bits in a byte.        */
+#define HWORD_BIT 16 /** Number of bits in a half-word.   */
+#define WORD_BIT 32  /** Number of bits in a word.        */
+#define DWORD_BIT 64 /** Number of bits in a double-word. */
 /**@}*/
 
 /**
  * @name Byte-Length of CPU Types
  */
 /**@{*/
-#define BYTE_SIZE 1  /** Byte        */
-#define HWORD_SIZE 2 /** Half Word   */
-#define WORD_SIZE 4  /** Word        */
-#define DWORD_SIZE 8 /** Double Word */
+#define BYTE_SIZE 1  /** Number of bytes in a byte.        */
+#define HWORD_SIZE 2 /** Number of bytes in a half-word.   */
+#define WORD_SIZE 4  /** Number of bytes in a word.        */
+#define DWORD_SIZE 8 /** Number of bytes in a double-word. */
 /**@}*/
+
+/*============================================================================*
+ * Types                                                                      *
+ *============================================================================*/
 
 #ifndef _ASM_FILE_
 
@@ -43,5 +63,9 @@ typedef uint64_t dword_t; /** Double Word */
 /**@}*/
 
 #endif /* _ASM_FILE_ */
+
+/*============================================================================*/
+
+/**@}*/
 
 #endif /* ARCH_X86_CPU_TYPES_H_ */
