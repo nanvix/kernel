e70c2d6d7d06c70dc9c2338ff9e6143dada0cccd
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2023-07-29 08:33:13 -0300

    [scripts] F: Adding Ubuntu Setup Script

---
+++ scripts/setup/ubuntu.sh
@@ -0,0 +1,21 @@
+# Copyright(c) 2011-2023 The Maintainers of Nanvix.
+# Licensed under the MIT License.
+
+# Update package repository.
+apt-get update
+
+# Install required packages.
+apt-get install -y   \
+    bison            \
+    build-essential  \
+    flex             \
+    libglib2.0-dev   \
+    libgmp-dev       \
+    libncurses5-dev  \
+    libncursesw5     \
+    libncursesw5-dev \
+    libpixman-1-dev  \
+    libsdl2-dev      \
+    ninja-build      \
+    pkg-config       \
+    texinfo          \
