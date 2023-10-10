1357423f0d96a95aeb50d38066f777d9035923d6
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2023-10-10 14:42:31 -0300

    [hal] E: Forge Interrupt Stack

--- src/kernel/hal/arch/int.c
+++ src/kernel/hal/arch/int.c
@@ -8,6 +8,7 @@
  *============================================================================*/
 
 #include <nanvix/kernel/hal.h>
+#include <nanvix/kernel/lib.h>
 
 /*============================================================================*
  * Public Variables                                                           *
@@ -32,3 +33,51 @@ interrupt_handler_t interrupt_handlers[INTERRUPTS_NUM] = {NULL,
                                                           NULL,
                                                           NULL,
                                                           NULL};
+
+/*============================================================================*
+ * Public Functions                                                           *
+ *============================================================================*/
+
+/**
+ * @details Forges an interrupt stack.
+ */
+void *interrupt_forge_stack(void *user_stack, void *kernel_stack,
+                            void (*user_func)(void), void (*kernel_func)(void))
+{
+    // Check for invalid user stack.
+    if (user_stack == NULL) {
+        kprintf("[hal] ERROR: invalid user stack");
+        return (NULL);
+    }
+
+    // Check for invalid kernel stack.
+    if (kernel_stack == NULL) {
+        kprintf("[hal] ERROR: invalid kernel stack");
+        return (NULL);
+    }
+
+    // Check for invalid user function.
+    if (user_func == NULL) {
+        kprintf("[hal] ERROR: invalid user function");
+        return (NULL);
+    }
+
+    // Check for invalid kernel function.
+    if (kernel_func == NULL) {
+        kprintf("[hal] ERROR: invalid kernel function");
+        return (NULL);
+    }
+
+    __memset(kernel_stack, 0, PAGE_SIZE);
+
+    // Create a fake interrupt stack.
+    word_t *kstackp = (word_t *)((word_t)kernel_stack + PAGE_SIZE);
+    *--kstackp = gdt_user_ds();       /* user ds     */
+    *--kstackp = (word_t)user_stack;  /* user esp    */
+    *--kstackp = EFLAGS_IF;           /* eflags      */
+    *--kstackp = gdt_user_cs();       /* cs          */
+    *--kstackp = (word_t)user_func;   /* user eip    */
+    *--kstackp = (word_t)kernel_func; /* kernel eip  */
+
+    return (kstackp);
+}
