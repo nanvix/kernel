deb429d6c8668effca634f5f27e872d93be21702
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2024-04-10 10:09:54 -0700

    [log] E: Convenient Log Macros

--- include/nanvix/kernel/log.h
+++ include/nanvix/kernel/log.h
@@ -50,7 +50,34 @@ extern int __log(const char *file, const char *funcname, unsigned level, ...);
  * @brief Prints a custom log message to the standard output.
  *
  * @param level The log message level e.g. `INFO`.
+ *
+ * @deprecated use `trace()`, `debug`, `info()`, `warn()` and `error()` instead.
  */
 #define log(level, ...) __log(__FILE__, __func__, level, __VA_ARGS__)
+
+/**
+ * @brief Prints a debug-level message on the standard output device.
+ */
+#define trace(...) log(TRACE, __VA_ARGS__)
+
+/**
+ * @brief Prints a debug-level message on the standard output device.
+ */
+#define debug(...) log(DEBUG, __VA_ARGS__)
+
+/**
+ * @brief Prints an information-level message on the standard output device.
+ */
+#define info(...) log(INFO, __VA_ARGS__)
+
+/**
+ * @brief Prints a warning-level message on the standard output device.
+ */
+#define warn(...) log(WARN, __VA_ARGS__)
+
+/**
+ * @brief Prints an error-level message on the standard output device.
+ */
+#define error(...) log(ERROR, __VA_ARGS__)
 
 #endif /* NANVIX_KERNEL_LOG_H_ */
