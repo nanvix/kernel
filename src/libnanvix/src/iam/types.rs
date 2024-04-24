b16e7552f1a9170e56cfe17b92d115576f26db3d
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2024-04-23 17:06:33 -0700

    [libnanvix] F: IAM Kernel Calls

---
+++ src/libnanvix/src/iam/types.rs
@@ -0,0 +1,14 @@
+/*
+ * Copyright(c) 2011-2024 The Maintainers of Nanvix.
+ * Licensed under the MIT License.
+ */
+
+//==============================================================================
+// Types
+//==============================================================================
+
+/// User ID.
+pub type UserID = i32;
+
+/// Group ID.
+pub type GroupID = i32;
