213a810b439b87d40761710a7857120a02471a30
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2024-04-30 17:59:24 -0700

    [test] E: Refactor Server Test

--- src/servers/test/src/lib.rs
+++ src/servers/test/src/lib.rs
@@ -10,7 +10,11 @@
 // Modules
 //==============================================================================
 
-mod test;
+pub mod ipc;
+pub mod kcall;
+pub mod misc;
+pub mod mm;
+pub mod pm;
 
 //==============================================================================
 // Imports
@@ -21,12 +25,37 @@ extern crate nanvix;
 use nanvix::power;
 
 //==============================================================================
+// Macros
+//==============================================================================
+
+///
+/// **Description**
+///
+/// Runs test and prints whether it passed or failed on the standard output.
+///
+#[macro_export]
+macro_rules! test {
+    ($fn_name:ident($($arg:expr),*)) => {{
+        match $fn_name($($arg),*) {
+            true =>
+                nanvix::log!("{} {}", "passed", stringify!($fn_name)),
+            false =>
+                panic!("{} {}", "FAILED", stringify!($fn_name)),
+        }
+    }};
+}
+
+//==============================================================================
 // Standalone Functions
 //==============================================================================
 
 #[no_mangle]
 pub fn main() {
     nanvix::log!("Running test server...");
-    test::test_kernel_calls();
+    kcall::test();
+    pm::test();
+    mm::test();
+    misc::test();
+    ipc::test();
     power::shutdown();
 }
