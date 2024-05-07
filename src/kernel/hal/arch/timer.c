68a660e6f08f03e1238825f4f3f6101f8afafdca
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2024-05-07 16:05:27 -0700

    [arch] E: Constants for Timer Driver

--- src/kernel/hal/arch/timer.c
+++ src/kernel/hal/arch/timer.c
@@ -34,6 +34,65 @@
 #define PIT_DATA 0x40 /** Data    */
 /**@}*/
 
+/**
+ * @name Select Channels
+ */
+/**@{*/
+#define PIT_SEL0 0x00 /** Channel 0         */
+#define PIT_SEL1 0x40 /** Channel 1         */
+#define PIT_SEL2 0x80 /** Channel 2         */
+#define PIT_RB 0xc0   /** Read-back command */
+/**@}*/
+
+/**
+ * @name Read-back Commands
+ */
+/**@{*/
+#define PIT_RB_CNTR0 0x02 /** Read-back channel 0      */
+#define PIT_RB_CNTR1 0x04 /** Read-back channel 1      */
+#define PIT_RB_CNTR2 0x08 /** Read-back channel 2      */
+#define PIT_RB_STAT 0x10  /** Don't latch status flag. */
+#define PIT_RB_COUNT 0x20 /** Don't latch count flag.  */
+/**@}*/
+
+/**
+ * @name Status Byte
+ */
+/**@{*/
+#define PIT_STAT_OUT 0x80  /** OUT pin status */
+#define PIT_STAT_NULL 0x00 /** NULL status    */
+/**@}*/
+
+/**
+ * @name Access Mode.
+ */
+/**@{*/
+#define PIT_ACC_LATCH 0x00 /** Latch count value          */
+#define PIT_ACC_LO 0x10    /** Access mode: lobyte only   */
+#define PIT_ACC_HI 0x20    /** Access mode: hibyte only   */
+#define PIT_ACC_LOHI 0x30  /** Access mode: lobyte/hibyte */
+/**@}*/
+
+/**
+ * @name Operating Mode
+ */
+/**@{*/
+#define PIT_MODE_TCOUNT 0x00   /** Mode 0: interrupt on terminal count      */
+#define PIT_MODE_HWSHOT 0x02   /** Mode 1: hardware re-triggerable one-shot */
+#define PIT_MODE_RATE 0x04     /** Mode 2: rate generator                   */
+#define PIT_MODE_WAVE 0x06     /** Mode 3: square wave generator            */
+#define PIT_MODE_SWSTROBE 0x08 /** Mode 4: software triggered strobe        */
+#define PIT_MODE_HWSTROBE 0x0a /** Mode 5: hardware triggered strobe        */
+/**@}*/
+
+/**
+ * @name BCD/Binary Mode
+ */
+/**@{*/
+#define PIT_BINARY 0x00 /** Binary mode */
+#define PIT_BCD 0x01    /** BCD mode    */
+/**@}*/
+
 /*============================================================================*
  * Public Functions                                                           *
  *============================================================================*/
