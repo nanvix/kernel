7aee878935a1c3d7cfdcc5c94d0e6492832181e7
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2024-05-12 12:38:44 -0700

    [kernel] E: Local Buffer for Logs

--- src/kernel/log/log.c
+++ src/kernel/log/log.c
@@ -33,15 +33,6 @@ static const char *MSG_LEVEL[LOG_LEVEL_MAX] = {
     "[TRACE]", "[DEBUG]", "[INFO]", "[WARN]", "[ERROR]"};
 
 /*============================================================================*
- * Private Variables                                                          *
- *============================================================================*/
-
-/**
- * @brief Message buffer.
- */
-static char buffer[KBUFFER_SIZE + 1];
-
-/*============================================================================*
  * Private Functions                                                          *
  *============================================================================*/
 
@@ -49,13 +40,14 @@ static char buffer[KBUFFER_SIZE + 1];
  * @brief Checks if the buffer can fit `ins_size` characters,
  * if not prints the current filled buffer.
  *
+ * @param buffer The buffer to store the log message.
  * @param buf_pos The next position to be inserted in the buffer.
  * @param ins_size The number of characters to be inserted in the buffer.
  *
  * @return The next position to be inserted in the buffer.
  *
  */
-static size_t chkbuf(size_t buf_pos, size_t ins_size)
+static size_t chkbuf(char *buffer, size_t buf_pos, size_t ins_size)
 {
     if (buf_pos > KBUFFER_SIZE) {
         buffer[KBUFFER_SIZE] = '\0';
@@ -76,16 +68,17 @@ static size_t chkbuf(size_t buf_pos, size_t ins_size)
  * @details The fuction calls `chkbuf()`,
  * thus it shall print the buffer to the standard output if the buffer is full.
  *
+ * @param buffer The buffer to store the log message.
  * @param s The source string to be copied.
  * @param i The position within the buffer to be inserted.
  *
  * @return The next position to be inserted in the buffer.
  *
  */
-static size_t cpy2buf(const char *s, size_t i)
+static size_t cpy2buf(char *buffer, const char *s, size_t i)
 {
     for (size_t j = 0; s[j] != '\0'; j++) {
-        i = chkbuf(i, 1);
+        i = chkbuf(buffer, i, 1);
         buffer[i++] = s[j];
     }
 
@@ -95,24 +88,26 @@ static size_t cpy2buf(const char *s, size_t i)
 /**
  * @brief Inserts module name and message level in the log message.
  *
+ * @param buffer The buffer to store the log message.
  * @param file A string defined by the macro `__FILE__`.
  * @param funcname A string defined by the macro `__func__`.
  * @param level The log message level.
  *
  * @return The next position to be inserted in the buffer.
  */
-static size_t fmt_prefix(const char *file, const char *funcname, int level)
+static size_t fmt_prefix(char *buffer, const char *file, const char *funcname,
+                         int level)
 {
     size_t i = 0;
 
     // Inserts message level.
-    i = cpy2buf(MSG_LEVEL[level], i);
+    i = cpy2buf(buffer, MSG_LEVEL[level], i);
 
     // Inserts module name.
-    i = cpy2buf(LOG_MODULE_SCOPE, i);
+    i = cpy2buf(buffer, LOG_MODULE_SCOPE, i);
     buffer[i++] = '[';
     for (size_t j = 0; file[j + 2] != '\0'; j++) {
-        i = chkbuf(i, 1);
+        i = chkbuf(buffer, i, 1);
         if (file[j] == '/') {
             buffer[i++] = ']';
             buffer[i++] = '[';
@@ -123,10 +118,10 @@ static size_t fmt_prefix(const char *file, const char *funcname, int level)
     buffer[i++] = ']';
 
     // Inserts function name.
-    i = chkbuf(i, __strlen(funcname) + 6);
+    i = chkbuf(buffer, i, __strlen(funcname) + 6);
     buffer[i++] = ' ';
-    i = cpy2buf(funcname, i);
-    i = cpy2buf("(): ", i);
+    i = cpy2buf(buffer, funcname, i);
+    i = cpy2buf(buffer, "(): ", i);
 
     return (i);
 }
@@ -140,19 +135,22 @@ static size_t fmt_prefix(const char *file, const char *funcname, int level)
  */
 int __log(const char *file, const char *funcname, unsigned level, ...)
 {
-    // Validades arguments.
+    // Validates arguments.
     if (file == NULL || funcname == NULL || level >= LOG_LEVEL_MAX) {
         return -1;
     }
 
-    va_list args;                                 // Variable arguments list
-    size_t i = fmt_prefix(file, funcname, level); // String length.
+    va_list args = {0};                  // Variable arguments list
+    char buffer[KBUFFER_SIZE + 2] = {0}; // Temporary buffer (+2 for \n\0).
+
+    // Build message prefix.
+    size_t i = fmt_prefix(buffer, file, funcname, level);
 
     va_start(args, level);
 
     // Print message into the buffer.
     const char *fmt_str = va_arg(args, char *);
-    i = chkbuf(i, __strlen(fmt_str));
+    i = chkbuf(buffer, i, __strlen(fmt_str));
     i += __vsnprintf(buffer + i, KBUFFER_SIZE - (i + 1), fmt_str, args);
 
     // Terminate string.
