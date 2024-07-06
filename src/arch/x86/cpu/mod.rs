3209d225b8c65525cdf3fa778b731350c68f2086
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2024-07-06 15:54:15 -0700

    [arch] F: Cpuid EDX Feature Detection

--- src/arch/x86/cpu/mod.rs
+++ src/arch/x86/cpu/mod.rs
@@ -6,6 +6,7 @@
 //==================================================================================================
 
 pub mod acpi;
+pub mod cpuid;
 pub mod eflags;
 pub mod excp;
 pub mod idt;
@@ -17,24 +18,3 @@ pub mod pit;
 pub mod ring;
 pub mod tss;
 pub mod xapic;
-
-//==================================================================================================
-// Standalone Functions
-//==================================================================================================
-
-///
-/// # Description
-///
-/// Checks if the CPU supports the CPUID instruction.
-///
-/// # Return Values
-///
-/// If the CPU supports the CPUID instruction, `true` is returned. Otherwise, `false` is returned.
-///
-pub fn has_cpuid() -> bool {
-    extern "C" {
-        fn __has_cpuid() -> bool;
-    }
-
-    unsafe { __has_cpuid() }
-}
--- src/hal/arch/x86/cpu/mod.rs
+++ src/hal/arch/x86/cpu/mod.rs
@@ -13,6 +13,7 @@ mod interrupt;
 //==================================================================================================
 
 use crate::{
+    arch::cpu::cpuid,
     config,
     error::Error,
     hal::{
@@ -59,6 +60,39 @@ pub fn init(
     extern "C" {
         static kstack: u8;
     }
+
+    // Print CPU features.
+    info!("CPU features:");
+    info!("  - has fpu:   {}", cpuid::has_fpu());
+    info!("  - has vme:   {}", cpuid::has_vme());
+    info!("  - has de:    {}", cpuid::has_de());
+    info!("  - has pse:   {}", cpuid::has_pse());
+    info!("  - has tsc:   {}", cpuid::has_tsc());
+    info!("  - has msr:   {}", cpuid::has_msr());
+    info!("  - has pae:   {}", cpuid::has_pae());
+    info!("  - has mce:   {}", cpuid::has_mce());
+    info!("  - has cx8:   {}", cpuid::has_cx8());
+    info!("  - has apic:  {}", cpuid::has_apic());
+    info!("  - has sep:   {}", cpuid::has_sep());
+    info!("  - has mtrr:  {}", cpuid::has_mtrr());
+    info!("  - has pge:   {}", cpuid::has_pge());
+    info!("  - has mca:   {}", cpuid::has_mca());
+    info!("  - has cmov:  {}", cpuid::has_cmov());
+    info!("  - has pat:   {}", cpuid::has_pat());
+    info!("  - has pse36: {}", cpuid::has_pse36());
+    info!("  - has psn:   {}", cpuid::has_psn());
+    info!("  - has clfsh: {}", cpuid::has_clflush());
+    info!("  - has ds:    {}", cpuid::has_ds());
+    info!("  - has acpi:  {}", cpuid::has_acpi());
+    info!("  - has mmx:   {}", cpuid::has_mmx());
+    info!("  - has fxsr:  {}", cpuid::has_fxsr());
+    info!("  - has sse:   {}", cpuid::has_sse());
+    info!("  - has sse2:  {}", cpuid::has_sse2());
+    info!("  - has ss:    {}", cpuid::has_ss());
+    info!("  - has htt:   {}", cpuid::has_htt());
+    info!("  - has tm:    {}", cpuid::has_tm());
+    info!("  - has ia64:  {}", cpuid::has_ia64());
+    info!("  - has pbe:   {}", cpuid::has_pbe());
 
     let (gdt, gdtr, tss) = unsafe { gdt::init(&kstack)? };
     unsafe { idt::init() };
