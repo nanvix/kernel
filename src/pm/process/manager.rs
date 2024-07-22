2bc57c38ef6e06b53869ee47646ae5b05a8006f5
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2024-07-22 21:43:00 +0000

    [mm] F: mmap Implementation

--- src/pm/process/manager.rs
+++ src/pm/process/manager.rs
@@ -621,6 +621,19 @@ impl ProcessManager {
         Ok(())
     }
 
+    pub fn mmap(
+        &mut self,
+        mm: &mut VirtMemoryManager,
+        pid: ProcessIdentifier,
+        vaddr: PageAligned<VirtualAddress>,
+        access: AccessPermission,
+    ) -> Result<(), Error> {
+        let mut pm: RefMut<ProcessManagerInner> = self.try_borrow_mut()?;
+        let mut process: ProcessRefMut = pm.find_process_mut(pid)?;
+        let vmem: &mut Vmem = process.state_mut().vmem_mut();
+        mm.alloc_upage(vmem, vaddr, access)
+    }
+
     ///
     /// # Description
     ///
