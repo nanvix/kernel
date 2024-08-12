f9e2f2cf6f7d603935ad68b8524bda8ad76247d4
Pedro Henrique Penna <ppenna@microsoft.com>
2024-08-12 07:36:43 -0700

    [build] F: Baremetal Machine

--- src/hal/arch/x86/platform/mod.rs
+++ src/hal/arch/x86/platform/mod.rs
@@ -5,13 +5,24 @@
 // Modules
 //==================================================================================================
 
+#[cfg(feature = "baremetal")]
+mod baremetal;
+
+#[cfg(any(feature = "isapc", feature = "pc"))]
 mod qemu;
 
 //==================================================================================================
 // Exports
 //==================================================================================================
 
+#[cfg(any(feature = "isapc", feature = "pc"))]
 pub use qemu::{
+    putb,
+    shutdown,
+};
+
+#[cfg(feature = "baremetal")]
+pub use baremetal::{
     putb,
     shutdown,
 };
